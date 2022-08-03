ff7e79d1419152b4ee113f14213f266502d2bef8
Junhyunny <kang3966@naver.com>
2022-08-03 21:16:55 +0900

    Finish Type Module

--- projects/basics-11-type-aliases/app.ts
+++ projects/basics-11-type-aliases/app.ts
@@ -1,49 +1,22 @@
-// define return type after function's params
-function addSecond(n1: number, n2: number): number {
-  return n1 + n2;
+// unkown is better than any. cause unknown restricts type checking at compile time
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+
+// error when userInput type is unknown
+// userName = userInput;
+
+if (typeof userInput == "string") {
+  userName = userInput;
 }
 
-// undefined is valid value and type in TypeScript
-
-// error when return type is undefined without return keyword
-// function printNumber(num: number): undefined {
-//   console.log("Result: " + num);
-// }
-
-// not error
-// function printNumber(num: number): undefined {
-//   console.log("Result: " + num);
-//   return;
-// }
-
-// void return undefined value
-function printNumber(num: number): void {
-  console.log("Result: " + num);
+function generateError(message: string, code: number): never {
+  throw {
+    errorCode: code,
+    message,
+  };
 }
 
-printNumber(addSecond(3, 5));
-
-// all function types are allowed
-// let combineVales: Function;
-
-// set parameter type and return type of function
-let combineVales: (n1: number, n2: number) => number;
-combineVales = addSecond;
-// combineVales = 1;
-// error - mismatch of param types and return type
-// combineVales = printNumber;
-
-printNumber(combineVales(3, 10));
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-addAndHandle(3, 5, printNumber);
-
-// callback functions can return something, even if the argument on which they're passed does NOT expect a returned value.
-addAndHandle(3, 5, (result) => {
-  console.log(result);
-  return "A";
-});
+generateError("Hello World", 500);
