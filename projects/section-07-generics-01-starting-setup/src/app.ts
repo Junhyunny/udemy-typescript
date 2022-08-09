675295905f1d3fd79801c60c0aa4cb7cff38f24c
Junhyunny <kang3966@naver.com>
2022-08-10 01:22:14 +0900

    Add Generics in TypeScript

--- projects/section-07-generics-01-starting-setup/src/app.ts
+++ projects/section-07-generics-01-starting-setup/src/app.ts
@@ -87,3 +87,48 @@ console.log(textStorage);
 // console.log(objectStorage.getItems());
 
 const numberStorage = new DataStorage<number>();
+
+interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string, completeUntil: Date): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = completeUntil;
+    return courseGoal as CourseGoal;
+}
+
+const userNames: Readonly<string[]> = ["Max", "Anna"];
+// compile error cause Readonly Utility Type is not allow to modify elements
+// userNames.push("Manu");
+// userNames.pop();
+
+// generic types versus union types
+// 제네릭을 안쓰고 union type으로 선언하면 값을 섞어 넣을 수 있게 된다.
+// 값을 섞어서 넣지 못하도록 데이터를 각 타입 별 어레이로 주면 값을 추가하거나 삭제하는데 문제가 된다.
+// 타입 가드가 필요하다.
+
+// class DataStorage<T extends string | number | boolean> {
+//     private data: T[] = [];
+
+//     addItem(item: T) {
+//         this.data.push(item);
+//     }
+
+//     // 함수에도 따로 타입을 지정 가능
+//     removeItem(item: T) {
+//         const index = this.data.indexOf(item);
+//         if (index === -1) {
+//             return;
+//         }
+//         this.data.splice(index, 1);
+//     }
+
+//     getItems(): T[] {
+//         return [...this.data];
+//     }
+// }
