dd1c75b01a6e6cc5b6efd9bf9e18c643043ec23c
Junhyunny <kang3966@naver.com>
2022-10-27 04:53:28 +0900

    Add 함수 파라미터 개수 변경

--- projects/chapter-11-basics-01-ts-benefits/app.ts
+++ projects/chapter-11-basics-01-ts-benefits/app.ts
@@ -1,14 +1,24 @@
-function add(n1: number, n2: number): number {
-    // typescript를 사용하면 불필요한 타입 확인 로직
+function add(n1: number, n2: number, printResult: boolean, resultPhrase: string) {
+    // typescript를 사용하면 불필요한 타입 확인 로직, Redundant
     // if (typeof n1 !== "number" || typeof n2 !== "number") {
     //     throw new Error("incorrect input error");
     // }
-    return n1 + n2;
+    // sometimes return or not
+    // it makes occur compile error when deciding return type
+    if (printResult) {
+        console.log(`${resultPhrase}${n1 + n2}`);
+    } else {
+        return n1 + n2;
+    }
 }
 
 const number1 = 5;
 const number2 = 2.8;
-const result = add(number1, number2);
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+//
+const result = add(number1, number2, printResult, resultPhrase);
 
 const element = document.getElementById("result") as HTMLElement | null;
 
