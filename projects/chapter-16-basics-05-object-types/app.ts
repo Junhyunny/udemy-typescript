e5a72693d803497c20974c0b68d7153095587825
Junhyunny <kang3966@naver.com>
2022-10-28 19:55:56 +0900

    Add TypeScript Literal Type

--- projects/chapter-16-basics-05-object-types/app.ts
+++ projects/chapter-16-basics-05-object-types/app.ts
@@ -1,69 +1,31 @@
-// 객체에 타입을 구체적으로 지정하는 방법, 구조체를 선언한다.
-// 좋은 플랙티스는 아니다.
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
-//     name: "Maximilian",
-//     age: 30,
-// };
-
-// 타입을 단순하게 object 타입으로 지정하는 방법
-// 객체 안에 어떤 값이 들어 있는지 모르기 때문에 IDE의 도움을 받을 수 없다.
-// const person: object = {
-//     name: "Maximilian",
-//     age: 30,
-// };
-
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string]; // tuple
-// } = {
-//     name: "Maximilian",
-//     age: 30,
-//     hobbies: ["Sports", "Cooking"],
-//     // if not assigned type
-//     // it would be (string | number) [] - union type
-//     role: [2, "author"],
-// };
-
-// 기본적으로 별도 값을 설정하지 않으면 자동 증가 숫자로 enum 값이 결정된다.
-// 어떠한 값이든 상관 없으므로 필요하다면 값을 할당해서 사용한다.
-// 특정 숫자가 지정되고 이 후에 다른 값을 지정하지 않았다면 자동적으로 증가하는 방식이다.
-enum Role {
-    ADMIN = "ADMIN",
-    READ_ONLY = 10,
-    AUTHOR,
+// | (pipeline)을 사용해서 타입을 두 개 이상 허용한다.
+// 파이프라인을 통해 타입을 합쳐서 사용하는 것을 Union 타입이라 한다.
+// 타입을 지정할 때 명시적인 값을 지정하는 경우 다른 값을 할당하면 컴파일 에러가 발생한다.
+function combine(
+    input1: number | string,
+    input2: number | string,
+    // 리터럴(literal) 타입 - 허용된 값만 매개변수로 전달 가능하다.
+    resultConversion: "as-number" | "as-text"
+): number | string {
+    let result;
+    if (
+        (typeof input1 === "number" && typeof input2 === "number") ||
+        // 오타를 쓰면 타입스크립트 컴파일러가 잡아준다.
+        resultConversion === "as-number"
+    ) {
+        // 앞에 +를 붙여주면 숫자가 된다
+        result = +input1 + +input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
 
-const person = {
-    name: "Maximilian",
-    age: 30,
-    hobbies: ["Sports", "Cooking"],
-    role: Role.ADMIN,
-};
+const combinedAge = combine(30, 26, "as-number");
+console.log(combinedAge);
 
-// TypeScript는 push 함수에 대해서 제어하지 못한다.
-// person.role.push("admin");
+const combinedStringAge = combine("30", "26", "as-number");
+console.log(combinedStringAge);
 
-// tuple로 선언하면 두번째 값에 지정된 타입의 값이 들어와야 컴파일 된다.
-// person.role[1] = 10;
-
-// push 함수에 의해서 추가되는 것은 감지하지 못하지만,할당하는 시점에는 컴파일 에러가 발생한다.
-// person.role = [0, "amdin", "user"];
-
-let favoriteActivities: string[];
-favoriteActivities = ["Sports"];
-
-// mixed array는 허용하지 않는다.
-// any type으로 우회할 수 있지만, TypeScript의 장점을 버리는 행위이다.
-// favoriteActivities = ["Sports", 1];
-
-console.log(person);
-
-// 배열의 타입을 알기 때문에 hobby는 string으로 사용할 수 있고, 자동 완성을 사용 가능하다.
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
-}
+const combinedNames = combine("Hello", "World", "as-text");
+console.log(combinedNames);
